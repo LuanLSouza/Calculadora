c738601b136e456cbe3051ff689391bb137fddad
Gabriela Mendes <gabrielamendes.tech@gmail.com>
2023-08-07 18:36:51 -0300

    Validador de operadores aritméticos

--- aula8.py
+++ aula8.py
@@ -1,14 +1,18 @@
 #Calculadora simples
 
-primeiro_numero = input ("Digite um número ")
+primeiro_numero = input ("Digite um número: ")
 while not primeiro_numero.isnumeric() :
     print("Você não digitou um número válido!")
-    primeiro_numero = input ("Digite um número ")
-operador = input ("Digite um operador aritmético ")
-segundo_numero = input ("Digite um número ")
+    primeiro_numero = input ("Digite um número: ")
+operadores_validos = ["+", "-", "*", "/"]    
+operador = input ("Digite um operador aritmético: ")
+if not operador in operadores_validos :
+    print("Você não digitou um operador aritmético válido!")
+    operador = input ("Digite um operador aritmético: ")
+segundo_numero = input ("Digite um número: ")
 while not segundo_numero.isnumeric() :
     print("Você não digitou um número válido!")
-    segundo_numero = input ("Digite um número ")
+    segundo_numero = input ("Digite um número: ")
 resultado = ...
 
 primeiro_numero = int(primeiro_numero)
@@ -25,7 +29,3 @@ if operador == "/":
 
 linha_resultado = f"{primeiro_numero} {operador} {segundo_numero} = {resultado}"
 print(linha_resultado)
-
-#digitar os numeros e operação em janela
-#tratar os imputs do usuário
-#ver números absolutos
