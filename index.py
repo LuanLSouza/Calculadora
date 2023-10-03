213ee65b79d33a53871b6996e8bc80d0748f6999
GabrielaMendex <gabrielamendes.tech@gmail.com>
2023-10-03 16:44:28 -0300

    organização de linhas

--- index.py
+++ index.py
@@ -1,19 +1,26 @@
 #Calculadora simples
 
 primeiro_numero = input ("Digite um número: ")
+
 while not primeiro_numero.isnumeric() :
     print("Você não digitou um número válido!")
     primeiro_numero = input ("Digite um número: ")
-operadores_validos = ["+", "-", "*", "/"]    
+
+operadores_validos = ["+", "-", "*", "/"] 
+
 operador = input ("Digite um operador aritmético: ")
+
 if not operador in operadores_validos :
     print("Você não digitou um operador aritmético válido!")
     operador = input ("Digite um operador aritmético: ")
+
 segundo_numero = input ("Digite um número: ")
+
 while not segundo_numero.isnumeric() :
     print("Você não digitou um número válido!")
     segundo_numero = input ("Digite um número: ")
-resultado = ...
+
+resultado = ...;
 
 primeiro_numero = int(primeiro_numero)
 segundo_numero = int(segundo_numero)
